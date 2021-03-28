d2bfac60dc792b6371e5c98d04e2b00a7c86b6d6
kamiv1980 <kamiv1980@gmail.com>
2021-03-28 14:08:03 +0300

    Remove fuction while

--- js/task-6.js
+++ js/task-6.js
@@ -1,19 +1,24 @@
-"use strict";
-console.log("Задание 6");
+'use strict'
+console.log('Задание 6')
 
-let input;
-let total=0;
-const numbers=[];
-do {
-    input=prompt('Please, input some number!');
-    const inputNumber = Number(input);
+let input
+let total = 0
+const numbers = []
+
+for (;;) {
+  input = prompt('Please, input some number!')
+  if (input !== null) {
+    const inputNumber = Number(input)
     if (Number.isNaN(inputNumber) === true) {
-      alert("This is not a number, please try again!");
+      alert('This is not a number, please try again!')
     } else {
-      numbers.push(inputNumber);
+      numbers.push(inputNumber)
     }
-  } while (input !== null);
-for (const number of numbers) {
-    total=total+number;
+  } else {
+    break
+  }
 }
-console.log(`The total sum of the numbers is ${total}`);
+for (const number of numbers) {
+  total = total + number
+}
+console.log(`The total sum of the numbers is ${total}`)
