3f434585544542a4194944f6e34b29267f8d1e52
kamiv1980 <kamiv1980@gmail.com>
2021-03-31 22:34:09 +0300

    Shorter code

--- js/task-5.js
+++ js/task-5.js
@@ -1,19 +1,12 @@
 'use strict'
 console.log('Задание 5')
 
-let resultOfCheck = false
 const checkForSpam = (message) => {
-  const words = message.split(' ')
-  for (const word of words) {
-    if (word.toLowerCase() === 'spam' || word.toLowerCase() === 'sale') {
-      resultOfCheck = true
-      console.log(resultOfCheck)
-      return resultOfCheck
-    }
-  }
+  const tempMessage = message.toLowerCase()
+  return tempMessage.includes('spam') || tempMessage.includes('sale')
 }
 
-checkForSpam('Latest technology news')
-checkForSpam('JavaScript weekly newsletter')
-checkForSpam('Get best sale offers now!')
-сheckForSpam('[SPAM] How to earn fast money?')
+console.log(checkForSpam('Latest technology news'))
+console.log(checkForSpam('JavaScript weekly newsletter'))
+console.log(checkForSpam('Get best sale offers now!'))
+console.log(checkForSpam('[SPAM] How to earn fast money?'))
